db980c9d5d6a4f5c9ee622fe718fc243d82b6b65
Bernardo Franco <berniifranco@gmail.com>
2022-12-29 02:11:45 -0300

    correcciones menores

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer () {
     return (
         <footer className="sticky-footer bg-white">
             <div className="container my-auto">
-					<div className="copyright text-center my-auto">
-						<span>Copyright &copy; Dashboard 2020</span>
-					</div>
-				</div>
+                <div className="copyright text-center my-auto">
+                    <span>Copyright &copy; Dashboard 2020</span>
+                </div>
+            </div>
         </footer>
     )
 }
